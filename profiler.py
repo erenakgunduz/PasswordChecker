d8d2d8623f5d29b6989ab7971fc9b3f106bc46db
erenakgunduz <coderenius@gmail.com>
2022-05-24 01:16:51 -0400

    Update profiler.py

--- profiler.py
+++ profiler.py
@@ -6,11 +6,11 @@ import passwordtool
 
 def main():
     with cProfile.Profile() as pr:
-        passwordtool.main()
+        passwordtool.collect_lists()
     stats = pstats.Stats(pr)
     stats.sort_stats(pstats.SortKey.TIME)
     # stats.print_stats()
-    stats.dump_stats(filename=f"{getcwd()}/profile-m.prof")
+    stats.dump_stats(filename=f"{getcwd()}/profile.prof")
 
 
 if __name__ == "__main__":
